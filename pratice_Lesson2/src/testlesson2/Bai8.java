2a82253497311a8d2f66309fac3ba911a98b6084
xuanhung2002 <xuanhung09052002@gmail.com>
2023-03-09 09:39:53 +0700

    lesson02-nguyenxuanhung

---
+++ pratice_Lesson2/src/testlesson2/Bai8.java
@@ -0,0 +1,72 @@
+package testlesson2;
+
+import java.util.Scanner;
+
+public class Bai8 {
+	private static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+	}
+
+//	private static boolean isValidNumber(String n) {
+//		try {
+//			if(Integer.parseInt(n) > 0) return true;
+//			else {
+//				return false;
+//			}
+//		} catch (Exception e) {
+//			return false;
+//		}
+//	}
+	private static boolean isValidNumber(String input) {
+		int textLength = input.length();
+		if (textLength == 0) {
+			return false;
+		}
+		for(int i = 0; i < textLength; i++) {
+//			if(input.charAt(i) < '0' || input.charAt(i) > '9') {
+//				return false;
+//			}
+			char letter = input.charAt(i);
+			if(!Character.isDigit(letter)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		String n;
+		int countError = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Practice 2: Check n is prime?");
+		System.out.print("Input n: ");
+		while (true) {
+			n = scanner.next();
+			
+			if (isValidNumber(n)) {
+				System.out.println(isPrime(Integer.parseInt(n)));
+				System.out.println("---End---");
+				break;
+			} else {
+				countError++;
+				if (countError == 5) {
+					System.out.println("You typed wrong 5 times");
+					System.out.println("---End---");
+					break;
+				}
+				System.out.print("Input n again: ");
+
+			}
+		}
+	}
+
+}
