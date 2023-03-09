76907f235f2de964571677aa3cd32ddfba37606c
xuanhung2002 <xuanhung09052002@gmail.com>
2023-03-09 19:21:54 +0700

    --

--- lesson2-data-structure/src/datastructure/array/Ex01BasicDemo.java
+++ lesson2-data-structure/src/datastructure/array/Ex01BasicDemo.java
@@ -1,5 +1,11 @@
 package datastructure.array;
 
-public class Ex01BasicDemo {
+import java.util.Arrays;
 
+public class Ex01BasicDemo {
+	public static void main(String[] args) {
+		int[] digits = new int[10];
+		System.out.println("digits -->" + Arrays.toString(digits));
+		System.out.println("digits length: " + digits.length);
+	}
 }
