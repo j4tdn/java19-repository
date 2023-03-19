66155cc629d0092a175f40eab59ed6da63caa5cb
Quyen Phan <Quyen.Phan@mgm-tp.com>
2023-03-19 17:28:13 +0700

    lesson03-oop 19.03.2023

--- lesson03-oop/src/bean/Ex02Test.java
+++
@@ -1,8 +0,0 @@
-package bean;
-
-public class Ex02Test {
-	public static void main(String[] args) {
-		Rectangle r1 = new Rectangle();
-		r1.width = 10;
-	}
-}
--- lesson03-oop/src/view/Ex01RectangleDemo.java
+++ lesson03-oop/src/view/Ex01RectangleDemo.java
@@ -8,7 +8,7 @@ public class Ex01RectangleDemo {
 		r1.setWidth(4);
 		r1.setHeight(2);
 		
-		r1.width = 10;
+		r1.setWidth(10);
 		System.out.println("r1 --> " + r1);
 		
 		Rectangle r2 = new Rectangle(8, 6);
