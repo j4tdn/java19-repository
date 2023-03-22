37c618ac90c7ef3d303e700abeb78f1f1ff26dd6
npq <npq2102@gmail.com>
2023-03-22 20:11:30 +0700

    Add BT2

---
+++ lesson03-nguyenphuquang/src/shopping/Order.java
@@ -0,0 +1,53 @@
+package shopping;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+public class Order {
+	private Customer customer;
+	private ItemDetail[] itemDetails;
+	private LocalDateTime orderDateTime;
+	
+	public Order() {
+
+	}
+
+	public Order(Customer customer, ItemDetail[] itemDetails, LocalDateTime orderDateTime) {
+		super();
+		this.customer = customer;
+		this.itemDetails = itemDetails;
+		this.orderDateTime = orderDateTime;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public ItemDetail[] getItemDetails() {
+		return itemDetails;
+	}
+
+	public void setItemDetails(ItemDetail[] itemDetails) {
+		this.itemDetails = itemDetails;
+	}
+
+	public LocalDateTime getOrderDateTime() {
+		return orderDateTime;
+	}
+
+	public void setOrderDateTime(LocalDateTime orderDateTime) {
+		this.orderDateTime = orderDateTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [customer=" + customer + ", itemDetails=" + Arrays.toString(itemDetails) + ", orderDateTime="
+				+ orderDateTime + "]";
+	}
+	
+	
+}
