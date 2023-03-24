07a9f1d6240ce055e892fa995ea58e9bafff1913
noo <ta642379@gmail.com>
2023-03-24 19:38:03 +0700

    lesson03-oop 03.20.2023

--- lesson02-data-structure/src/view/Ex01App.java
+++ lesson02-data-structure/src/view/Ex01App.java
@@ -2,6 +2,26 @@ package view;
 
 public class Ex01App {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println(solution("OXXXOXOOX"));
+		System.out.println(solution("XOXXXOOOXOOO"));
+		System.out.println(solution("XX"));
+		System.out.println(solution("XXOOOOOXX"));
 	}
+	public static String solution(String S) {
+		if (S.length() < 3) return "tie";
+        char winningPlayer = '\0';  
+        int count = -1;
+        for (int i = 0; i < S.length() - 2; i++) {
+            char curr = S.charAt(i);
+            if (curr == S.charAt(i+1) && curr == S.charAt(i+2)) {
+            	if(winningPlayer != curr) count++;
+            	winningPlayer = curr;
+            }
+        }
+        if (count > 0) {
+            return "tie";
+        } else {
+            return String.valueOf(winningPlayer);
+        }
+    }
 }
---
+++ lesson03-oop/src/bean/Rectangle.java
@@ -0,0 +1,46 @@
+package bean;
+
+public class Rectangle {
+	
+	// access modifier
+	// private: trong class hiện tại
+	// public : mọi nơi
+	//        : trong package chứa class
+	
+	// attribute: thuộc tính
+	public int width;
+	public int height;
+
+	// 
+	public Rectangle() {
+
+	}
+	
+	public Rectangle(int width, int height) {
+		this.width  = width;
+		this.height = height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.width + ", " + this.height;
+	}
+	
+}
