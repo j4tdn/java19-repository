8b044756dd7db7d3bc21cc27594811a2395a1584
nguyenphuquang <npq2102@gmail.com>
2023-04-27 20:48:47 +0700

    Add Bai5

--- lesson05-nguyenphuquang/src/bai5/App.java
+++ lesson05-nguyenphuquang/src/bai5/App.java
@@ -5,13 +5,14 @@ public class App {
 	public static void main(String[] args) {
 
 	}
-public static Book[] initBook() {
-	Book[] books = new Book[5];
-	books[0] = new ClassicalBook("SGK1",10000,"Giao Duc",true);
-	books[1] = new ClassicalBook("SGK2",15000,"Giao Duc",false);
-	books[2] = new ClassicalBook("SGK3",20000,"Tuoi Tre",true);
-	books[3] = new ReferenceBook("STK1",52000,"Văn Hóa",2.3);
-	books[4] = new ReferenceBook("STK2",67000,"Thông Tin",5.3);
-	return books;
-}
+
+	public static Book[] initBook() {
+		Book[] books = new Book[5];
+		books[0] = new ClassicalBook("SGK1", 10, "Giao Duc", true);
+		books[1] = new ClassicalBook("SGK2", 150, "Nhi Dong", false);
+		books[2] = new ClassicalBook("SGK3", 250, "Tuoi Tre", true);
+		books[3] = new ReferenceBook("STK1", 120, "Nhi Dong", 2.3);
+		books[4] = new ReferenceBook("STK2", 30, "Nhi Dong", 5.3);
+		return books;
+	}
 }
