efedb5071a6de40680615937354ded06575d2dd7
tuanviet <tuanvietnguyen03@gmail.com>
2023-05-25 20:10:46 +0700

    lesson06 25.05.2023

---
+++ lesson06-exceptions/src/throw_throws_trycatch/Ex03MultipleException.java
@@ -0,0 +1,44 @@
+package throw_throws_trycatch;
+
+import java.util.Scanner;
+
+public class Ex03MultipleException {
+	private static Scanner ip = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// Lưu ý khi try nhiều catch
+		// Khối catch đứng trước/trên phải là
+		// 1. không liên quan gì đến khối catch đứng sau
+		// 2. con của khối catch phía sau
+		
+		try {
+			int result = inputAndDivide();
+			System.out.println("result --> " + result);
+		} catch (NumberFormatException | ArithmeticException e) {
+			System.out.println("exception --> " + e.getMessage());
+		}
+		
+		System.out.println("*** ___ finished ___ ***");
+
+		ip.close();
+	}
+
+	private static int inputAndDivide() {
+		int a = input("Enter A");
+		int b = input("Enter B");
+		return divide(a, b);
+	}
+
+	private static int input(String prefix) {
+		System.out.print(prefix + ": ");
+		return Integer.parseInt(ip.nextLine());
+	}
+
+	private static int divide(int a, int b) {
+		if (b == 0) {
+			throw new ArithmeticException("b should not be zero !!!");
+		}
+		return a / b;
+	}
+
+}
---
+++ lesson06-exceptions/src/validate_trycatch/Ex01YearOfBirth_Validation.java
@@ -0,0 +1,90 @@
+package validate_trycatch;
+
+import java.time.Year;
+import java.util.Scanner;
+
+/**
+ * Yêu cầu: Nhập vào năm sinh (số nguyên & <= năm hiện tại) Tính tuổi của ứng
+ * viên
+ * 
+ * @author Admin
+ *
+ */
+
+public class Ex01YearOfBirth_Validation {
+	public static void main(String[] args) {
+		System.out.println("\\");
+		//[0-9]	--> chuỗi có phải số nguyên ko
+		//\d 	--> chuỗi có phải số nguyên ko
+		// --> [0-9] ~ \d
+		// bỏ dấu \ ban đầu => \\d
+		//{1,} 	--> yêu cầu 1 hay nhiều kí tự
+		//+ 	--> yêu cầu 1 hay nhiều kí tự
+		System.out.println("1 -->" + ("a").matches("[0-9]"));
+		System.out.println("2 -->" + ("5").matches("[0-9]"));
+		System.out.println("3 -->" + ("&").matches("[0-9]"));
+		System.out.println("4 -->" + ("&").matches("[0-9]"));
+		System.out.println("5 -->" + ("&").matches("[0-9]"));
+		System.out.println("===");
+		System.out.println("1 -->" + ("a").matches("\\d"));
+		System.out.println("2 -->" + ("5").matches("\\d"));
+		System.out.println("3 -->" + ("&").matches("\\d"));
+		System.out.println("4 -->" + ("&").matches("\\d"));
+		System.out.println("5 -->" + ("&").matches("\\d"));
+		System.out.println("===");
+		System.out.println("5 -->" + ("19").matches("[1-8][0-9]"));
+		System.out.println("5 -->" + ("08").matches("[1-8][0-9]"));
+		System.out.println("5 -->" + ("148").matches("[1-8]{3}"));
+		System.out.println("5 -->" + ("14834").matches("[1-8]{3,5}"));
+		System.out.println("5 -->" + ("1434128731").matches("[1-8]{1,}"));
+		System.out.println("5 -->" + ("14934128731").matches("\\d+"));
+	
+		Scanner ip = new Scanner(System.in);
+		int currentYear = Year.now().getValue();
+		System.out.println("=======");
+		System.out.println("current year: " + currentYear);
+		System.out.print("Enter year of birth: ");
+		
+		String yobAsString = "";
+		//validate yobAsString là 1 chuỗi số hợp lệ trước khi
+		//convert sang int 
+		
+		do {
+			yobAsString = ip.nextLine();
+			//stringVariable.matches(regular expression || regex pattern || biểu thức chính quy)
+			// biểu thức chính quy (string): là 1 bộ/ tập hợp các cú pháp / từ điển để kiểm tra 
+			//tính hợp lệ của 1 chuỗi string
+			
+			//VD: kiểm tra có phải chuỗi số hợp lệ hay ko?
+			//chuỗi số hợp lệ: ít nhất 1 kí tự
+			//					tất cả phải là số nguyên
+			if (yobAsString.matches("\\d+")) {
+				break;
+			}
+			System.out.println("please enter valid number(s)");
+		} while (true);
+		
+		int yob = Integer.parseInt(ip.nextLine());
+		int yourAge = currentYear - yob + 1;
+		System.out.println("your age: " + yourAge);
+		System.out.println("=======");
+		ip.close();
+		
+		/*
+		 * JVM ném ra jhi code bị lỗi tại dòng 21
+		 * Exception in thread "main" java.lang.NumberFormatException: For input string: "asd"
+	at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:67)
+	at java.base/java.lang.Integer.parseInt(Integer.java:668)
+	at java.base/java.lang.Integer.parseInt(Integer.java:786)
+	at validate_trycatch.Ex01YearOfBirth.main(Ex01YearOfBirth.java:21)
+			
+		 */
+		/*
+		 * Thay vì dừng chương trình khi bị ngoại lệ thì:
+		 * 	In thông tin lỗi và yêu cầu người dùng thao tác 
+		 * 	Tối đa n lần -> thoát chương trình
+		 *  
+		 */
+	}
+
+}
