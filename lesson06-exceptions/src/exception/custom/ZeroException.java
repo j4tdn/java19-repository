0a5602e28bde73a9fa9a61924c0437458310d789
Truong Cong Trinh <trinhtruongcong3@gmai.com>
2023-05-25 18:35:59 +0700

    lesson06-exceptions 20.05.2023

---
+++ lesson06-exceptions/src/exception/custom/ZeroException.java
@@ -0,0 +1,12 @@
+package exception.custom;
+
+public class ZeroException extends Exception{
+	
+	// java seriallizable, read/write data
+	private static final long serialVersionUID = -1032930172131826808L;
+
+	public ZeroException(String message) {
+		super(message);
+	}
+	
+}
