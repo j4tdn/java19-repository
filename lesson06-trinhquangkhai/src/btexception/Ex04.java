bf46955247bae58b92080226613d03d7eafa1a78
qkhai <quangkhai251004@gmail.com>
2023-06-01 18:45:27 +0700

    update lesson06

--- lesson06-trinhquangkhai/src/btexception/Ex04.java
+++ lesson06-trinhquangkhai/src/btexception/Ex04.java
@@ -1,57 +1,69 @@
 package btexception;
 
-
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ex04 {
-public static void main(String[] args) {
-	Scanner ip = new Scanner(System.in);
-	System.out.println("Enter the number");
-	int num = Integer.valueOf(ip.nextInt());
-	readNumber(num);
-}
-private static String[] readNumber( int number) {
-	String digits [] = {
-			"Không", "Một", "Hai", "Ba", " Bốn",
-			"Năm", "Sáu", "Bảy", "Tám", "Chín", "Mười",
-			"Mười Một", "Mười Hai", "Mười Ba", "Mười Bốn",
-			"Mười lăm ", "Mười Sáu", "Mười Bảy", "Mười Tám",
-			"Mười Chín"
-	};
-	String ten[] = {
-			"Hai Mươi", "Ba Mươi", "Bốn Mươi", "Năm Mươi", "Sáu Mươi",
-			"Bảy Mươi", "Tám Mươi","Chín Mươi"
-	};
-	if(number < 20) {
-		System.out.println(digits[number]);
-	}else if(number < 100) {
-		int tendigits = number/10;
-		int onedigits = number%10;
-		if(onedigits == 0) {
-			System.out.println(ten[tendigits]);
-		}else {
-			System.out.println(ten[tendigits] + " " + digits[onedigits]);
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		while (true) {
+			try {
+				System.out.println("Enter the number");
+				int num = ip.nextInt();
+				ReadNumber(num);
+				break;
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid number!Please try again");
+				continue;
+			}
 		}
-	}else {
-		int hundredDigits = number/100;
-		int tendigits = (number/10)%10;
-		int onedigits = number % 10;
-		if(tendigits == 0 && onedigits == 0) {
-			System.out.println(digits[hundredDigits] + "Một Trăm");
-		}else if(tendigits == 0) {
-			System.out.println(digits[hundredDigits] + "Một Trăm" + digits[onedigits]);
-		}else if(tendigits == 1) {
-			System.out.println(digits[hundredDigits] + "Một Trăm" + digits[tendigits * 10 + onedigits]);
-		}else if(onedigits == 0) {
-			System.out.println(digits[hundredDigits] + "Một Trăm" + ten[tendigits]);
-		}else {
-			System.out.println(digits[hundredDigits] + "Một Trăm" + ten[tendigits] + " " + digits[onedigits]);
-		}
-	}
-	
-	return digits;
-	
-}
-}
 
+	}
+
+	private static String ReadNumber(int num) {
+		if (num < 0 || num > 999) {
+			throw new IllegalArgumentException("Number must be between 0 to 999");
+		}
+		String st = "";
+		String[] one = { "Không", "Một", "Hai", "Ba", "Bốn", "Năm", "Sáu", "Bảy", "Tám", "Chín" };
+		String[] ten = { "Mười", "Hai Mươi", "Ba Mươi", "Bốn Mươi", "Năm Mươi", "Sáu Mươi", "Bảy Mươi", "Tám Mươi",
+				"Chín Mươi" };
+		String[] hundred = { "Một Trăm", "Hai Trăm", "Ba Trăm", "Bốn Trăm", "Năm Trăm", "Sáu Trăm", "Bảy Trăm",
+				"Tám Trăm", "Chín Trăm" };
+		if (num < 10) {
+			System.out.println(one[num]);
+		} else if (num < 20) {
+			if (num == 15) {
+				st = "Mười Lăm";
+			} else if (num == 10) {
+				st = "Mười";
+			} else {
+				st = "Mười" + one[num];
+			}
+
+		} else if (num < 100) {
+			int tendigit = (num / 10);
+			int onedigit = num % 10;
+			if (onedigit == 0) {
+				st = ten[tendigit];
+			} else {
+				st = ten[tendigit] + " " + one[onedigit];
+			}
+		} else if (num < 1000) {
+			int tendigit = (num / 10) % 10;
+			int onedigit = num % 10;
+			if (tendigit == 0 && onedigit == 0) {
+				st = hundred[num];
+			} else if (num % 100 < 10) {
+				st = hundred[num] + " LẺ " + one[onedigit];
+			} else if (num % 100 < 20) {
+				st = hundred[num] + " Mười " + one[onedigit];
+			} else {
+				st = hundred[num] + ten[tendigit] + " " + one[onedigit];
+			}
+		}
+		return st.trim();
+	}
+
+}
