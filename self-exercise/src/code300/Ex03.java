b9f5cf16779a9bf03e6468000846b8001c39e255
qkhai <quangkhai251004@gmail.com>
2023-06-08 19:04:21 +0700

    bt3

--- self-exercise/src/code300/Ex03.java
+++ self-exercise/src/code300/Ex03.java
@@ -1,13 +1,43 @@
 package code300;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ex03 {
 public static void main(String[] args) {
-	
-}
-public String longestCommonPrefix(String[] strs) {
-	for(String str : strs) {
-	for(int i = 0; i < strs.length; i++) {
-		if(str[i])
+	Scanner ip = new Scanner(System.in); 
+	String st;
+	System.out.println("Nhập số phần tử của mảng");
+	int n = ip.nextInt();
+	String []sts = new String[n];
+ 	while(true) {
+	try {
+		for(int i = 0; i < n; i++) {
+			System.out.println("Nhập chuỗi thứ " + i);
+			sts[i] = ip.next();
+			System.out.println("Các phần tử đó là: ");
+			for(String e : sts) {
+			System.out.println(e);
+			}
+		}
+		System.out.println("các phần tử chung là: " + longestCommonPrefix(sts));
+			}
+		
+	catch(IllegalArgumentException e) {
+		System.out.println(e.getMessage());
 	}
-}
 }}
+public static String longestCommonPrefix(String[] strs) {
+	
+		String result = strs[0];
+	for(int i = 1; i< strs.length; i++) {
+	 if(strs[i].startsWith(result)){
+		result = result.substring(0,strs.length);
+	} if (result.isEmpty()){
+		result = "";
+	}
+	}
+	return result;
+
+}}
+
