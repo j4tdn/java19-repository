90ab6a9ff3cd6aeca7b1fa698f30bfdf218bfa14
nguyenphuquang <npq2102@gmail.com>
2023-06-22 21:19:24 +0700

    add Ex03

--- lesson09-nguyenphuquang/src/ex03/Ex03Demo.java
+++ lesson09-nguyenphuquang/src/ex03/Ex03Demo.java
@@ -6,23 +6,24 @@ public class Ex03Demo {
 		String string = "Welcome   to   JAVA10   class";
 		System.out.println(revertString(string));
 	}
-public static String revertString(String s) {
-	String[] words = s.split("[\\s,]+");
-	String result = "";
-	for(String word : words) {
-		result = result.concat(revertWord(word) + " ");
-	}
-	
-	return result;
-}
 
-public static String revertWord(String s) {
-	String result = "";
-	for(int i = s.length(); i > 0;i--) {
-		result = result + (s.charAt(i-1));
+	public static String revertString(String s) {
+		String[] words = s.split("[\\s,]+");
+		String result = "";
+		for (String word : words) {
+			result = result.concat(revertWord(word) + " ");
+		}
+
+		return result;
 	}
-	
-	return result;
-}
+
+	public static String revertWord(String s) {
+		String result = "";
+		for (int i = s.length(); i > 0; i--) {
+			result = result + (s.charAt(i - 1));
+		}
+
+		return result;
+	}
 
 }
