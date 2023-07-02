6ad7e5dbb2d23a77e65113b6d76f6422772b2d16
Quyen Phan <Quyen.Phan@mgm-tp.com>
2023-07-02 23:12:11 +0700

    lesson09-qphan 02.07.2023

--- lesson09-qphan/src/view/test01/Ex0NTestFormatter.java
+++ lesson09-qphan/src/view/test01/Ex0NTestFormatter.java
@@ -1,15 +1,69 @@
 package view.test01;
 
-import java.util.Arrays;
-
 public class Ex0NTestFormatter {
+	
+	private static final int DEFAULT_SEPRATED_SPACES = 5;
+	
 	public static void main(String[] args) {
 		String[][] a2D = { 
-				{ "1aaaaaa",   "2yyyyyy",       "32222222" }, // row[0]
-				{ "12",        "2uuuuuuuuuuuu", "3ttttttttttttttt" }, // row[1]
-				{ "1aaaaaa33", "2yyyyyy",       "3mmmmmmmm22222" } // row[2]
+				{ "123"  , "123"    , "123456" }, // row[0]
+				{ "12345", "1234567", "123" },    // row[1]
+				{ "12"   , "12345"  , "123" }     // row[2]
 		};
-
+		printTable(a2D);
+		
+		System.out.println("\n==========================\n");
+		
+		printTableWithSignSeparator(a2D);
+	}
+	
+	private static void printTableWithSignSeparator(String[][] a2D) {
+		int[] maxLengthByColumn = findMaxLengthForEachColumn(a2D);
+		int rowLength = a2D[0].length;
+		int contentLength = 0;
+		for (int rowI = 0; rowI < a2D.length; rowI++) {
+			String content = "| ";
+			for (int colI = 0; colI < rowLength; colI++) {
+				String spaces = generateSpaces(maxLengthByColumn[colI] - a2D[rowI][colI].length()) + " | ";
+				content = content + a2D[rowI][colI] + spaces;
+			}
+			contentLength = content.length();
+			printSeparatedLine(contentLength);
+			System.out.println(content);
+			
+		}
+		printSeparatedLine(contentLength);
+	}
+	
+	
+	private static void printTable(String[][] a2D) {
+		int[] maxLengthByColumn = findMaxLengthForEachColumn(a2D);
+		int rowLength = a2D[0].length;
+		for (int rowI = 0; rowI < a2D.length; rowI++) {
+			for (int colI = 0; colI < rowLength; colI++) {
+				String spaces = generateSpaces(maxLengthByColumn[colI] - a2D[rowI][colI].length() + DEFAULT_SEPRATED_SPACES);
+				System.out.print(a2D[rowI][colI] + spaces);
+			}
+			System.out.println();
+		}
+	}
+	
+	private static void printSeparatedLine(int length) {
+		for (int i = 0; i < length - 1; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	}
+	
+	private static String generateSpaces(int amount) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < amount; i++) {
+			sb.append(" ");
+		}
+		return sb.toString();
+	}
+	
+	private static int[] findMaxLengthForEachColumn(String[][] a2D) {
 		int[] maxLengthByColumn = new int[a2D.length];
 		int rowLength = a2D[0].length;
 		for (int colI = 0; colI < rowLength; colI++) {
@@ -21,32 +75,6 @@ public class Ex0NTestFormatter {
 			}
 			maxLengthByColumn[colI] = maxLength;
 		}
-		
-		System.out.println("\n\n");
-		
-		int defaultSeparateSpaces = 20;
-		
-		for (int colI = 0; colI < rowLength; colI++) {
-			for (int row = 0; row < a2D.length; row++) {
-				String spaces = generateSpaces(maxLengthByColumn[colI] - a2D[row][colI].length() + defaultSeparateSpaces);
-				
-				// System.out.println("value: " + a2D[row][colI]);
-				// System.out.println("value length: " + a2D[row][colI].length());
-				// System.out.println("max as col: " + maxLengthByColumn[colI]);
-				// System.out.println("plus spaces --> " + (maxLengthByColumn[colI] - a2D[row][colI].length() + defaultSeparateSpaces));
-				
-				System.out.print(a2D[colI][row] + spaces);
-			}
-			System.out.println();
-		}
-		
-	}
-	
-	private static String generateSpaces(int amount) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < amount; i++) {
-			sb.append(" ");
-		}
-		return sb.toString();
+		return maxLengthByColumn;
 	}
 }
