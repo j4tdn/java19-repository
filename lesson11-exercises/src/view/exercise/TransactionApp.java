fccf728764c68963a6e657c0671df2977834ce65
xuanhung2002 <xuanhung09052002@gmail.com>
2023-08-17 21:50:36 +0700

    lesson 11 ex online 17/8/2023

--- lesson11-exercises/src/view/exercise/TransactionApp.java
+++ lesson11-exercises/src/view/exercise/TransactionApp.java
@@ -2,7 +2,11 @@ package view.exercise;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class TransactionApp {
 	
@@ -16,27 +20,37 @@ public class TransactionApp {
 //			System.out.println(transaction);
 //		}
 		
+		System.out.println("++++++++1+++++++");
 		printf(ex1(transactions));
-		System.out.println("+++++++++++++++");
 		// 2. Find all transactions have value greater than 300 and sort them by trader’s city
+		System.out.println("+++++++2+++++++");
 		printf(ex2(transactions));
-		System.out.println("+++++++++++++++");
 		// 3. What are all the unique cities where the traders work?
-		
+		System.out.println("++++++++3++++++");
+		ex3(transactions).forEach(System.out::println);
 		// 4. Find all traders from Cambridge and sort them by name desc.
+		System.out.println("++++++++4++++++");
 		printf(ex4(transactions));
-		System.out.println("+++++++++++++++");
+		
 		// 5. Return a string of all traders’ names sorted alphabetically.
-
+		System.out.println("++++++++5++++++");
+		ex5(transactions).forEach(System.out::println);
 		// 6. Are any traders based in Milan?
-			ex06(transactions);
+		System.out.println("++++++++6+++++++");
+			System.out.println(ex6(transactions));
+			
 		// 7. Count the number of traders in Milan.
-		
+			System.out.println("++++++++7+++++++");
+			System.out.println(ex7(transactions));
 		// 8. Print all transactions’ values from the traders living in Cambridge.
-		
+			System.out.println("+++++++8++++++++");
+			ex8(transactions).forEach(System.out::println);
 		// 9. What’s the highest value of all the transactions?
-		
+			System.out.println("+++++++9++++++++");
+			System.out.println(ex9(transactions));
 		// 10. Find the transaction with the smallest value
+			System.out.println("+++++++10++++++++");
+			System.out.println(ex10(transactions));
 		
 		
 	}
@@ -63,6 +77,13 @@ public class TransactionApp {
 		return result;
 	}
 	
+	private static Set<String> ex3(List<Transaction> transactions){
+		Set<String> result = new HashSet<String>();
+		for (Transaction transaction : transactions) {
+			result.add(transaction.getTrader().getLivingCity());
+		}
+		return result;
+	}
 	
 	private static List<Transaction> ex4(List<Transaction> transactions){
 		List<Transaction> result = new ArrayList<Transaction>();
@@ -76,17 +97,15 @@ public class TransactionApp {
 	}
 	
 	
-	private static List<String> ex5(List<Trader> traders){
-		List<String> result = new ArrayList<String>();
-		for (Trader trader : traders) {
-			result.add(trader.getFullname());
+	private static Set<String> ex5(List<Transaction> transactions){
+		Set<String> result = new TreeSet<>();
+		for (Transaction transaction : transactions) {
+			result.add(transaction.getTrader().getFullname());
 		}
-		result.sort(Comparator.comparing(String::toString));
-		
 		return result;
 	}
 	
-	private static boolean ex06(List<Transaction> transactions) {
+	private static boolean ex6(List<Transaction> transactions) {
 		for (Transaction transaction : transactions) {
 			if(transaction.getTrader().getLivingCity().equals("Milan")) {
 				return true;
@@ -94,6 +113,48 @@ public class TransactionApp {
 		}
 		return false;
 	}
+	
+	private static int ex7(List<Transaction> transactions) {
+		int result = 0;
+		for (Transaction transaction : transactions) {
+			if(transaction.getTrader().getLivingCity().equals("Milan")) {
+				result++;
+			}
+		}
+		return result;
+	}
+	
+	
+	private static List<Long> ex8(List<Transaction> transactions){
+		List<Long> result = new ArrayList<>();
+		for (Transaction transaction : transactions) {
+			if(transaction.getTrader().getLivingCity().equals("Cambridge")) {
+				result.add(transaction.getValue());
+			}
+		}
+		return result;
+	}
+	
+	private static Long ex9(List<Transaction> transactions) {
+		Long result = Long.MIN_VALUE;
+		for(Transaction transaction:transactions) {
+			if(transaction.getValue() > result) {
+				result = transaction.getValue();
+			}
+		}
+		return result;
+	}
+	
+	public static Transaction ex10(List<Transaction> transactions) {
+		Transaction result = transactions.get(0);
+		for (Transaction transaction : transactions) {
+			if(transaction.getValue() < result.getValue()) {
+				result = transaction;
+			}
+		}
+		return result;
+	}
+	
 	
 	public static void printf(List<Transaction> transactions) {
 		for (Transaction transaction : transactions) {
