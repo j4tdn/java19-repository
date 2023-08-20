8b3b46b9de219cce7363e3d0dc1d56d2be4ce04d
Truong Cong Trinh <trinhtruongcong3@gmai.com>
2023-08-20 15:42:46 +0700

    lesson11-truongcongtrinh 20.08.2023

---
+++ lesson11-truongcongtrinh/src/view/Ex01TwoOppositeTriangles.java
@@ -0,0 +1,7 @@
+package view;
+
+public class Ex01TwoOppositeTriangles {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ lesson11-truongcongtrinh/src/view/Ex02CDStoreManagementDemo.java
@@ -0,0 +1,25 @@
+package view;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex02CDStoreManagementDemo {
+	private static final Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+	}
+//	
+//	private static void addCD(List<CD> CDs) {
+//		
+//	}
+	
+	
+//	private static List<CD> mockData() {
+//		return List.of(
+//					new CD(),
+//					new CD()
+//				);
+//	}
+	
+}
