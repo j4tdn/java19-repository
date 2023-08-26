475e22be35e80a329b408efa457eb353e8df5f00
Truong Cong Trinh <trinhtruongcong3@gmai.com>
2023-08-26 14:07:17 +0700

    lesson12-stream 26.08.2023

--- lesson12-stream/src/view/knowledge/Ex99ReviewFunctionalInterface.java
+++ lesson12-stream/src/view/knowledge/Ex99ReviewFunctionalInterface.java
@@ -1,21 +1,18 @@
 package view.knowledge;
 
+import java.util.function.BiPredicate;
 
 public class Ex99ReviewFunctionalInterface {
 	 public static void main(String[] args) {
 		 
+		 // TestFunctionalInterface #abstract-method# T apply(U u,V v);  ~ BiFunction/
 		TestFunctionalInterface<Integer, Integer, Integer> t = new TestFunctionalInterface<Integer, Integer, Integer>() {
-			
 			@Override
 			public Integer apply(Integer u, Integer v) {
 				return u+v;
 			}
 		};
 		
-		TestFunctionalInterface<Integer, Integer, Integer> t2 = (u,v) -> u+v;
-		
-		System.out.println(t.apply(2, 3));
-		System.out.println(t2.apply(2, 3));
 		
 		
 		
