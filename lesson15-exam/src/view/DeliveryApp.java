5e92cbc05233b341c89b471d94fd008e5f86e1f6
Hoang Nam <namnguyenduchoang@gmail.com>
2023-09-21 22:59:48 +0700

    lesson15-exam

---
+++ lesson15-exam/src/view/DeliveryApp.java
@@ -0,0 +1,76 @@
+package view;
+
+import bean.Item;
+import bean.Store;
+import model.DataModel;
+import utils.NumberUtils;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+public class DeliveryApp {
+
+    public static void main(String[] args) {
+        checkPlanningAmount();
+
+        fillGapsWithReferencesOrAverage();
+    }
+
+    private static void checkPlanningAmount() {
+        List<Store> storesA55 = DataModel.mockStoresOfRefItemA55();
+
+        for (Store store : storesA55) {
+            BigDecimal planningAmount = store.getStorePotential();
+
+            System.out.println("Planning Amount for Store " + store.getStoreId() + ": " + planningAmount);
+
+            if (planningAmount.compareTo(NumberUtils.bd(100)) <= 0) {
+                System.out.println("Calculation stopped for Store " + store.getStoreId());
+                break;
+            }
+        }
+    }
+
+    private static void fillGapsWithReferencesOrAverage() {
+        List<Store> storesA55 = DataModel.mockStoresOfRefItemA55();
+
+        Map<Integer, Integer> refStores = DataModel.mockRefStores();
+
+        Map<Integer, BigDecimal> refWeights = DataModel.mockRefWeights();
+
+        for (Store store : storesA55) {
+            BigDecimal potential = store.getStorePotential();
+
+            if (potential == null || potential.compareTo(BigDecimal.ZERO) == 0) {
+                Integer refStoreId = refStores.get(store.getStoreId());
+
+                if (refStoreId != null) {
+                    for (Store refStore : storesA55) {
+                        if (refStore.getStoreId().equals(refStoreId)) {
+                            store.setStorePotential(refStore.getStorePotential());
+                            break;
+                        }
+                    }
+                } else {
+                    BigDecimal sumPotential = BigDecimal.ZERO;
+                    int count = 0;
+                    for (Store avgStore : storesA55) {
+                        BigDecimal avgPotential = avgStore.getStorePotential();
+                        if (avgPotential != null && avgPotential.compareTo(BigDecimal.ZERO) != 0) {
+                            sumPotential = sumPotential.add(avgPotential);
+                            count++;
+                        }
+                    }
+
+                    if (count > 0) {
+                        BigDecimal averagePotential = sumPotential.divide(BigDecimal.valueOf(count), 1, BigDecimal.ROUND_HALF_UP);
+                        store.setStorePotential(averagePotential);
+                    }
+                }
+
+                System.out.println("Potential for Store " + store.getStoreId() + " filled: " + store.getStorePotential());
+            }
+        }
+    }
+}
