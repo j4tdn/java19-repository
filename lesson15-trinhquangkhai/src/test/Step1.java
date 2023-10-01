75c6f176503eff98dee5ce889b2efce29ae29dc8
Quyen Phan <Quyen.Phan@mgm-tp.com>
2023-10-01 09:28:32 +0700

    Kết quả review bạn Quang Khải
    + Em chỉ mới hoàn thành step 1, step 2 đang code giữa chừng chưa được
    + Code chưa tốt Khải nghe
    
    --> Tạm thời thì a sẽ đánh giá fail và tham gia lại lớp khác :/, a sẽ trao đổi sau em nhé

--- lesson15-trinhquangkhai/src/test/Step1.java
+++ lesson15-trinhquangkhai/src/test/Step1.java
@@ -7,60 +7,66 @@ import java.util.Map;
 import bean.Store;
 import model.DataModel;
 
+// Tên class không được đặt như này
+// A có giải thích là code trong class DeliveryApp như trong code a rồi
 public class Step1 {
 	public static void main(String[] args) {
-// step 1: Check for manually added amounts 
-		int PlanningAmount = 10; 
-		if(PlanningAmount <= 0) {
+		
+		// step 1: Check for manually added amounts 
+		int PlanningAmount = 10;
+		if (PlanningAmount <= 0) {
 			System.out.println("Planning amount must be greater than 0. ");
 			return;
 		}
-			List<Store> storesA55 = DataModel.mockStoresOfRefItemA55();
-			List<Store> storesA77 = DataModel.mockStoresOfRefItemA77();
+		// Sao học đến giờ rồi mà code ko format vậy em ?
+		List<Store> storesA55 = DataModel.mockStoresOfRefItemA55();
+		List<Store> storesA77 = DataModel.mockStoresOfRefItemA77();
 
-	        Map<Integer, Integer> refStores = DataModel.mockRefStores();
-	        Map<Integer, BigDecimal> refWeights = DataModel.mockRefWeights();
-	        Map<Integer, BigDecimal> storeTrendFactors = DataModel.mockStoreTrendFactors();
+		Map<Integer, Integer> refStores = DataModel.mockRefStores();
+		Map<Integer, BigDecimal> refWeights = DataModel.mockRefWeights();
+		Map<Integer, BigDecimal> storeTrendFactors = DataModel.mockStoreTrendFactors();
 
-	        allocateProducts(storesA55, refStores, refWeights, storeTrendFactors);
+		// Code chưa tốt nơi Khải nghe :/
+		allocateProducts(storesA55, refStores, refWeights, storeTrendFactors);
 
-	        for (Store store : storesA55) {
-	            System.out.println("Store " + store.getStoreId() + ": " + store.getItem().getItenName()
-	                    + " - Allocated Quantity: " + store.getStorePotential());
-	        }
-	    }
+		for (Store store : storesA55) {
+			System.out.println("Store " + store.getStoreId() + ": " + store.getItem().getItenName()
+					+ " - Allocated Quantity: " + store.getStorePotential());
+		}
+	}
 
-	    private static void allocateProducts(List<Store> stores, Map<Integer, Integer> refStores,
-	                                         Map<Integer, BigDecimal> refWeights, Map<Integer, BigDecimal> storeTrendFactors) {
-	        for (Store store : stores) {
-	            int refStoreId = refStores.get(store.getStoreId());
-	            BigDecimal refWeight = refWeights.get(store.getItem().getItemId());
-	            BigDecimal trendFactor = storeTrendFactors.get(store.getStoreId());
+	private static void allocateProducts(List<Store> stores, Map<Integer, Integer> refStores,
+			Map<Integer, BigDecimal> refWeights, Map<Integer, BigDecimal> storeTrendFactors) {
+		for (Store store : stores) {
+			int refStoreId = refStores.get(store.getStoreId());
+			BigDecimal refWeight = refWeights.get(store.getItem().getItemId());
+			BigDecimal trendFactor = storeTrendFactors.get(store.getStoreId());
 
-	            BigDecimal allocationQuantity = calculateAllocationQuantity(refStoreId, refWeight, trendFactor);
-	            allocationQuantity = allocateWithinStoreLimit(allocationQuantity);
+			BigDecimal allocationQuantity = calculateAllocationQuantity(refStoreId, refWeight, trendFactor);
+			allocationQuantity = allocateWithinStoreLimit(allocationQuantity);
 
-	            store.setStorePotential(allocationQuantity);
-	        }
-	    }
+			store.setStorePotential(allocationQuantity);
+		}
+	}
 
-	    private static BigDecimal calculateAllocationQuantity(int refStoreId, BigDecimal refWeight, BigDecimal trendFactor) {
-	        BigDecimal allocationQuantity = refWeight.multiply(trendFactor);
-	        if (refStoreId == 10) {
-	            allocationQuantity = allocationQuantity.multiply(BigDecimal.valueOf(0.8));
-	        }
-	        return allocationQuantity;
-	    }
+	private static BigDecimal calculateAllocationQuantity(int refStoreId, BigDecimal refWeight,
+			BigDecimal trendFactor) {
+		BigDecimal allocationQuantity = refWeight.multiply(trendFactor);
+		if (refStoreId == 10) {
+			allocationQuantity = allocationQuantity.multiply(BigDecimal.valueOf(0.8));
+		}
+		return allocationQuantity;
+	}
 
-	    private static BigDecimal allocateWithinStoreLimit(BigDecimal allocationQuantity) {
-	        if (allocationQuantity.compareTo(BigDecimal.valueOf()) > 0) {
-	            return BigDecimal.valueOf();
-	        }
-	        return allocationQuantity;
-	    }
-	    {
-		}}
-	
-		
-	
-	
+	private static BigDecimal allocateWithinStoreLimit(BigDecimal allocationQuantity) {
+		// BigDecimal.valueOf() cần truyền tham số vào
+		// Ví dụ BigDecimal.valueOf(20) chứ e để vậy là lỗi compile rồi
+		if (allocationQuantity.compareTo(BigDecimal.valueOf()) > 0) {
+			return BigDecimal.valueOf();
+		}
+		return allocationQuantity;
+	}
+
+	{
+	}
+}
