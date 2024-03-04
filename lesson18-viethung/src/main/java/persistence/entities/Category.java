51c9ff84e3f12cf2bbd0bd63ea7b90239c184ad7
Hung Le <you@example.com>
2024-03-04 21:35:22 +0700

    lesson18-exam 04-03-2024

---
+++ lesson18-viethung/src/main/java/persistence/entities/Category.java
@@ -0,0 +1,41 @@
+package persistence.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Item> items;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
