68abbf369bcc84b66f1005813878f3959b1e204e
vanthinh <ta642379@gmail.com>
2024-03-04 21:37:38 +0700

    lesson 18 exam tran van thinh

---
+++ bkit.java.1920/src/main/java/dao/ItemDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import persistence.dto.ItemByDateDto;
+import persistence.entities.Item;
+
+public interface ItemDao {
+	List<ItemByDateDto> getItemByDate(LocalDate localDate);
+	List<String> bai3(LocalDate localDate);
+}
---
+++ bkit.java.1920/src/main/java/service/ItemService.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import persistence.dto.ItemByDateDto;
+import persistence.entities.Item;
+import persistence.entities.ItemGroup;
+
+public interface ItemService {
+	List<ItemByDateDto> getItemsByDate(LocalDate localDateTime);
+	List<String> bai3(LocalDate localDate); 
+}
